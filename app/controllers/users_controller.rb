4db36c78c7cb0b5ea3257a213764e9b7decbf0c2
Anton Lipich <anton.lipich1@gmail.com>
2021-04-09 18:40:09 +0300

    add user params & routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,12 +47,12 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.fetch(:user, {})
-    end
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
